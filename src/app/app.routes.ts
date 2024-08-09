bb3a3174eea8308de20e6021abce8d03fe4d1776
Sneha S. <104543929+snehasadap@users.noreply.github.com>
2024-08-08 22:24:40 -0400

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { FacialRecognitionComponent } from './facial-recognition/facial-recognition.component';
-import { ImageDetectionComponent } from './image-detection/image-detection.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: '/webcam-detection', pathMatch: 'full' },
-  { path: 'webcam-detection', component: FacialRecognitionComponent, data: { title: 'Webcam Detection' } },
-  { path: 'image-detection', component: ImageDetectionComponent, data: { title: 'Image Detection' } }
-];
